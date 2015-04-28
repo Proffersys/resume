eda643f6a8ee89a32b5aa42a55e86700724cb0d6
guptaarchit <guptaarchit21@gmail.com>
2015-04-28 07:56:19 +0530

    Update analysis.php

--- dashboard/analysis.php
+++ dashboard/analysis.php
@@ -1,9 +1,20 @@
-<?php include 'header.php';?>
+<?php include 'header.php';
+?>
 
       <!-- **********************************************************************************************************************************************************
       MAIN SIDEBAR MENU
       *********************************************************************************************************************************************************** -->
 <!--sidebar start-->
+        <style>
+                .profcontent{
+                    min-height: 910px;
+                }
+                #d2{
+                    border-bottom: black;
+                    z-index: 1px;
+                    box-shadow: 2px; 
+                }
+        </style>
 <aside>
 <div id="sidebar"  class="nav-collapse ">
     <!-- sidebar menu start-->
@@ -62,7 +73,109 @@
   </ul>
     <!-- sidebar menu end-->
 </div>
-</aside>
+</aside>    
+        
+
+        <script src='assets/js/chart.min.js'></script>
+        <!-- pie chart canvas element -->
+ 
+            
+            <div id='d2' style="position:relative; left:210px;background: #FFFFFF">
+            <canvas id="jobs" width="300" height="400"></canvas>
+            <div style="position:absolute;top:100px;left:600px;border:1px solid red;text-align:center;padding: 10px 4px;width:200px;background: #FFFFFF">
+            <b>JOBS AVAILABLE AS PER LANGUAGE IN 2013</b><br>
+            <span style="color:#878BB6">SQL</span><br>
+            <span style="color:#4ACAB4">JAVA</span><br>
+            <span style="color:#FF8153">HTML</span><br>
+            <span style="color:#FFEA88">JAVASCRIPT</span><br>
+            <span style="color:#00FF00">C++</span><br>
+            <span style="color:#996633">C#</span><br>
+            <span style="color:#660066">XML</span><br>
+            <span style="color:#FF0099">C</span><br>
+            <span style="color:#333300">PERL</span><br>
+            <span style="color:#CC0000">PYTHON</span>       
+            <!-- bar chart canvas element --></div></div>
+            <br>
+            <div id='d3' style="position:relative; left:210px;background: #FFFFFF">
+        <canvas id="salary" width="600" height="400" left="230px"></canvas>
+         <div style="position:absolute;top:150px;left:600px;border:1px solid red;text-align:center;padding: 10px 4px;width:200px;background: #FFFFFF">
+            <span style="color:#660066">x-Axis:Skills</span><br>
+            <span style="color:#660066">y:Axis:salary in $ per annum</span><br>     
+            <!-- bar chart canvas element --></div>
+        </div>
+        <script>
+            // pie chart data
+            var pieData =[
+                {
+                    value: 98454,
+                    color:"#878BB6"
+                },
+                {
+                    value : 66485,
+                    color : "#4ACAB4"
+                },
+                {
+                    value : 45456,
+                    color : "#FF8153"
+                },
+                {
+                    value : 43189,
+                    color : "#FFEA88"
+                },
+                {
+                    value : 32018,
+                    color : "#00FF00"
+                },
+                {
+                    value : 31936,
+                    color : "#996633"
+                },
+                {
+                    value : 31431,
+                    color : "#660066"
+                },
+                {
+                    value : 24801,
+                    color : "#FF0099"
+                },
+                {
+                    value : 20579,
+                    color : "#333300"
+                },               
+                {
+                    value : 19627,
+                    color : "#CC0000"
+                }
+            ];
+            // pie chart options
+            var pieOptions = {
+                 segmentShowStroke : false,
+                 animateScale : true
+            }
+            // get pie chart canvas
+            var jobs= document.getElementById("jobs").getContext("2d");
+            // draw pie chart
+            new Chart(jobs).Pie(pieData, pieOptions);
+            // bar chart data
+            </script>
+                    
+            <script>
+            var barData = {
+                labels : ["SQL  ","JAVA","HTML","JAVASCRIPT","C++","C#","XML","C","PERL","PYTHON"],
+                datasets : [
+                    {
+                        fillColor : "#FF0000",
+                        strokeColor : "#48A4D1",
+                        data : [90000,95000,81000,81000,94000,91000,92000,93000,93000,83000]
+                    }
+                ]
+            }
+            // get bar chart canvas
+            var salary = document.getElementById("salary").getContext("2d");
+            // draw bar chart
+            new Chart(salary).Bar(barData);
+        </script><br>
+
 <!--sidebar end-->
       
       <!-- **********************************************************************************************************************************************************
